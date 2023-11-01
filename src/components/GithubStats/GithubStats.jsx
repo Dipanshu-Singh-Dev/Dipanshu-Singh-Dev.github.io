9478733f8090e915b8d4d1d53030526b5756f418
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-11-02 00:08:13 +0530

    github section responsive

--- src/components/GithubStats/GithubStats.jsx
+++ src/components/GithubStats/GithubStats.jsx
@@ -1,6 +1,7 @@
 import Separator from '../Separator/Separator'
 import styles from "./GithubStats.module.css"
 import Head1 from "../Head1/Head1"
+import GitHubCalendar from "react-github-calendar-dipanshu";
 const GithubStats = () => {
   return (
     <div className={styles.container}>
@@ -19,8 +20,18 @@ const GithubStats = () => {
         <img
           id="normalLangs"
           className={styles.StatImages}
-          src="https:/is-potato2.vercel.app/api/top-langs/?username=dipanshu-singh-dev&layout=donut&langs_count=4&theme=transparent&text_color=d3d3d3&ring_color=ffffff&title_color=ffffff"
+          src="https:/is-potato2.vercel.app/api/top-langs/?username=dipanshu-singh-dev&layout=donut&langs_count=4&theme=transparent&text_color=d3d3d3&title_color=ffffff"
           alt="langs"
+        />
+      </div>
+      <div id={styles.calendar}>
+        <h2 style={{ color: "white" }}>Github Calendar</h2>
+        <GitHubCalendar
+          showWeekdayLabels
+          weekStart="1"
+          username="dipanshu-singh-dev"
+          colorScheme="dark"
+          style={{ color: "white",width:"90%",margin:'auto' }}
         />
       </div>
       <Separator />
