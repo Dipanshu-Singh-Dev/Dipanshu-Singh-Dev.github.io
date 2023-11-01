d2e1794366d4403d19450f4f23cbc35fd92995b9
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.com>
2023-11-01 19:14:36 +0530

    added : responsiveness

--- src/components/GithubStats/GithubStats.jsx
+++ src/components/GithubStats/GithubStats.jsx
@@ -8,18 +8,18 @@ const GithubStats = () => {
       <div className={styles.SVGContainer}>
         <img
           className={styles.StatImages}
-          src="https://is-potato2.vercel.app/api?username=dipanshu-singh-dev&theme=transparent&text_color=d3d3d3&border_radius=2&ring_color=ffffff&custom_title=Github Stats&title_color=ffffff&show_icons=true&icon_color=ffffff"
+          src="https://is-potato2.vercel.app/api?username=dipanshu-singh-dev&theme=transparent&text_color=d3d3d3&ring_color=ffffff&custom_title=Github Stats&title_color=ffffff&show_icons=true&icon_color=ffffff"
           alt="stats"
         />
         <img
           className={styles.StatImages}
-          src="https://is-potato.vercel.app?user=dipanshu-singh-dev&theme=transparent&date_format=j%20M%5B%20Y%5D&sideNums=white&stroke=gray&border=darkgray&ring=lightgray&fire=white&currStreakNum=white&currStreakLabel=white&sideLabels=white&dates=lightgray"
+          src="https://is-potato.vercel.app?user=dipanshu-singh-dev&theme=transparent&date_format=j%20M%5B%20Y%5D&sideNums=white&stroke=gray&border=white&ring=lightgray&fire=white&currStreakNum=white&currStreakLabel=white&sideLabels=white&dates=lightgray"
           alt="streak"
         />
         <img
           id="normalLangs"
           className={styles.StatImages}
-          src="https:/is-potato2.vercel.app/api/top-langs/?username=dipanshu-singh-dev&layout=donut&langs_count=4&theme=transparent&text_color=d3d3d3&border_radius=2&ring_color=ffffff&title_color=ffffff"
+          src="https:/is-potato2.vercel.app/api/top-langs/?username=dipanshu-singh-dev&layout=donut&langs_count=4&theme=transparent&text_color=d3d3d3&ring_color=ffffff&title_color=ffffff"
           alt="langs"
         />
       </div>
