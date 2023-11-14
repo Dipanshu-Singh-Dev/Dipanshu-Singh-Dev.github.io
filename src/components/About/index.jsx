2405138c3400a3805440a87ed10abc1f645b7485
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2023-11-15 04:28:57 +0530

    only links and classes ids left

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import styles from "./styles.module.css"
 import Head1 from "../Head1/Head1"
 import Seperator from "../Separator/Separator"
+import BlackButton from '../Button/black'
+import WhiteButton from "../Button/white";
 const index = () => {
   return (
     <div id="about" className={styles.container}>
@@ -13,20 +15,19 @@ const index = () => {
           src="https://i.ibb.co/hBvkVkx/profile-removebg-preview.png"
           alt="profile"
         />
-        <div>
+        <div id={styles.aboutDetails}>
           <h2 id="user-detail-name" className={styles.head2}>
             Hi! I'm Dipanshu Singh.
           </h2>
           <p id="user-detail-intro" className={styles.intro}>
             <span className={styles.position}>Full Stack Web Developer</span>{" "}
-            with proficiency in working with interdisciplinary teams and
-            executing goal-oriented projects. Demonstrated expertise in writing
-            production-ready code using React for frontend development, as well
-            as Node.js and Express for backend implementation. Skilled in
-            Generative AI and Prompt Engineering, with a strong passion for
-            coding and a keen interest in contributing to a progressive
-            organization.
+            with proficiency in executing goal-oriented projects. Demonstrated expertise in writing
+            production-ready code using MERN Stack.
           </p>
+          <div id={styles.buttonContainer}>
+            <BlackButton text="Github" external />
+            <WhiteButton external text="Resume" />
+          </div>
         </div>
       </div>
       <Seperator />
