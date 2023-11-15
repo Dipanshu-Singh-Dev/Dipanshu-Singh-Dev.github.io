dd3a6a9b4b350b88cf4608a3742fbbf61c961192
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2023-11-15 19:24:13 +0530

    draft

--- src/components/GithubStats/GithubStats.jsx
+++ src/components/GithubStats/GithubStats.jsx
@@ -9,16 +9,18 @@ const GithubStats = () => {
       <div className={styles.SVGContainer}>
         <img
           className={styles.StatImages}
+          id="github-stats-card"
           src="https://is-potato2.vercel.app/api?username=dipanshu-singh-dev&theme=transparent&text_color=d3d3d3&ring_color=ffffff&custom_title=Github Stats&title_color=ffffff&show_icons=true&icon_color=ffffff"
           alt="stats"
         />
         <img
+          id="github-streak-stats"
           className={styles.StatImages}
           src="https://is-potato.vercel.app?user=dipanshu-singh-dev&theme=transparent&date_format=j%20M%5B%20Y%5D&sideNums=white&stroke=gray&border=white&ring=lightgray&fire=white&currStreakNum=white&currStreakLabel=white&sideLabels=white&dates=lightgray"
           alt="streak"
         />
         <img
-          id="normalLangs"
+          id="github-top-langs"
           className={styles.StatImages}
           src="https://is-potato2.vercel.app/api/top-langs/?username=dipanshu-singh-dev&layout=donut&langs_count=4&theme=transparent&text_color=d3d3d3&title_color=ffffff"
           alt="langs"
