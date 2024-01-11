2aea86f45e4cb4d48304b806f0d5e41f11d5222a
Dipanshu-Singh-Dev <work.dipanshu.singh@gmail.comnode>
2024-01-11 12:06:21 +0530

    added lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 // App.jsx
+import React, { lazy, Suspense } from "react";
 import Navbar from "./components/Navbar/Navbar";
-import About from "./components/About";
-import Skills from "./components/Skills/Skills";
 import BlurryBlob from "./components/BlurryBlob";
-import GithubStats from "./components/GithubStats/GithubStats";
-import Projects from "./components/Projects/Projects";
-import Contact from "./components/Contact";
+import About from "./components/About";
+const Skills = lazy(() => import("./components/Skills"));
+const Projects = lazy(() => import("./components/Projects"));
+const Contact = lazy(() => import("./components/Contact"));
+const GithubStats = lazy(() => import("./components/GithubStats"));
 
 function App() {
   return (
@@ -13,12 +14,23 @@ function App() {
       <BlurryBlob />
       <Navbar />
       <div id="app">
-      <div style={{height:'1px'}} id="home"></div>
-        <About />
-        <Skills />
-        <GithubStats />
-        <Projects />
-        <Contact />
+        <div style={{ height: "0px" }} id="home"></div>
+          <About />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Skills />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <GithubStats />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Projects />
+        </Suspense>
+
+        <Suspense fallback={<div>Loading...</div>}>
+          <Contact />
+        </Suspense>
       </div>
     </>
   );
